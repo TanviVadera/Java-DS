c2bb976ce3e236a1b63227404eca1e84b4299610
Tanvi Vadera <tanvivadera12@gmail.com>
2026-06-20 17:12:18 +0530

    FactorialRecursion.java

---
+++ Recursion/FactorialRecursion.java
@@ -0,0 +1,13 @@
+public class FactorialRecursion {
+    public static int Fact(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * Fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(Fact(n));
+    }
+}
